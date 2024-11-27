50124a4873f4fa96603c5d728286473024e0f558
Priti-Pawar <pritipawar161299@gmail.com>
2024-11-27 21:40:13 +0530

    Adding string exntension for right substring methods

---
+++ GenericsAndExtensionMethods/StringExtensions.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GenericsAndExtensionMethods
+{
+    public static class StringExtensions
+    {
+        public static string RightSubstring(this string str,int count) 
+        {
+            return str.Substring(str.Length - count, count);
+        }
+    }
+}
