8bff5dbc575c5abf96788cc45a1da15ea0077995
Priti-Pawar <pritipawar161299@gmail.com>
2024-11-27 22:23:22 +0530

    Adding Generic method to compare string or integer

--- GenericsAndExtensionMethods/Program.cs
+++ GenericsAndExtensionMethods/Program.cs
@@ -4,8 +4,36 @@
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("*********** String extension method to return Right Substring *************************");
             string sampleStr = "GHOST";
-            Console.WriteLine(sampleStr.RightSubstring(3));
+            int count = 3;
+            Console.WriteLine("String:" + sampleStr + "\nCount:" + count + "\nRightSubstring:" + sampleStr.RightSubstring(3));
+
+            Console.WriteLine("---------------------------------------------------------------------------------------");
+
+            Console.WriteLine("*********** Generic method to check if two strings or integers are equal **************");
+
+            int a = 2, b = 2;
+            if(Generics.AreEqual(a, b)) 
+            {
+                Console.WriteLine(a + " and " + b + " are equal");
+            }
+            else
+            {
+                Console.WriteLine(a + " and " + b + " are not equal");
+            }
+
+            string c = "GHOST", d = "GHOSTT";
+            if (Generics.AreEqual(c, d)) 
+            {
+                Console.WriteLine(c + " and " + d + " are equal");
+            }
+            else 
+            {
+                Console.WriteLine(c + " and " + d + " are not equal");
+            }
+            Console.WriteLine("---------------------------------------------------------------------------------------");
+
         }
     }
 }
